a3842c1765bb5d7de303019577cff914c0d146f6
Noumal <44552446+Noumal@users.noreply.github.com>
2018-10-28 21:34:29 +0300

    Update encodeArray.java
    Initially, the encoding number was reversed. Therefore, I reversed it again in a way that we can get:
    the number 1234 encoded as the array {0,1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1}

--- src/com/knight/exam/java/encodeArray/EncodeArray.java
+++ src/com/knight/exam/java/encodeArray/EncodeArray.java
@@ -1,11 +1,9 @@
-package com.knight.exam.java.encodeArray;
-
 import java.util.Arrays;
-
 /**
- * Created by sachinkeshav on 1/3/15.
+ * Created by sachinkeshavé on 1/3/15.  
+ *  Modified by noumal on 28/10/2018
  */
-public class EncodeArray {
+public class Exo {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(encodeArray(0)));
@@ -21,27 +19,34 @@ public class EncodeArray {
 
         int size = n < 0 ? 1 : 0;
         int nCopy = n < 0 ? -n : n;
+        int nbDigit = 0;
 
         while (nCopy > 0) {
             int digit = nCopy % 10;
             nCopy /= 10;
+            nbDigit++;
             size += digit + 1;
         }
 
         int[] result = new int[size];
+        int[] temp = new int[nbDigit];
 
         nCopy = n < 0 ? -n : n;
+
+        for (int x = temp.length - 1; x >= 0; x--) {
+            temp[x] = nCopy % 10;
+            nCopy /= 10;
+        }
+
         for (int i = 0; i < size; i++) {
             if (i == 0 && n < 0) {
                 result[i] = -1;
                 i++;
             }
 
-            while (nCopy > 0) {
-                int digit = nCopy % 10;
-                nCopy /= 10;
+            for (int y = 0; y < temp.length; y++) {
 
-                for (int j = 0; j < digit; j++) {
+                for (int j = 0; j < temp[y]; j++) {
                     result[i] = 0;
                     i++;
                 }
@@ -52,3 +57,6 @@ public class EncodeArray {
         return result;
     }
 }
+
+
+
